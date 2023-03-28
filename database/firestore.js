4501a45190e318a4f3abb580800289996b54e5b5
Elias Vasquez <evasq710@hotmail.com>
2023-03-28 01:40:33 -0600

    Insert query

--- database/firestore.js
+++ database/firestore.js
@@ -2,7 +2,7 @@ const db = firebase.firestore();
 
 export async function insert(item) {
     try {
-        const response = await db.collection("banco").add(errorNew);
+        const response = await db.collection("banco").add(item);
         return response;
     } catch (error) {
         throw new Error(error);
